a060c42e60f541d56f38f255de2828e85ec1ec6b
root <root@ip-172-31-45-244.ec2.internal>
2024-03-06 06:27:38 +0000

    store to s3

--- sc.sh
+++ sc.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 WP_FOLDER="/var/www/html/wordpress"
-WP_CONFIG="${WP_FOLDER}/wp-config.php"
-echo "WP_FOLDER: $WP_FOLDER"
-echo "WP_CONFIG: $WP_CONFIG"
+#WP_CONFIG="${WP_FOLDER}/wp-config.php"
+#echo "WP_FOLDER: $WP_FOLDER"
+#echo "WP_CONFIG: $WP_CONFIG"
 
+DB_NAME="wp_db"
+DB_USER="wp_user"
+DB_PASSWORD="wordpress"
+DB_HOST="localhost"
+#DB_NAME=$(grep -E "^define\('DB_NAME'" "$WP_CONFIG" |  cut -d"'" -f 4)
+#DB_USER=$(grep -E "^define\('DB_USER'" "$WP_CONFIG" |  cut -d"'" -f 4)
+#DB_PASSWORD=$(grep -E "^define\('DB_PASSWORD'" "$WP_CONFIG" |  cut -d"'" -f 4)
+#DB_HOST=$(grep -E "^define\('DB_HOST'" "$WP_CONFIG" | cut -d"'" -f 4)
 
-if [ ! -f "$WP_CONFIG" ]; then
-    echo "wp-config.php not found in $WP_FOLDER"
-    exit 1
-fi
+#echo "DB_USER: $DB_USER"
+#echo "DB_NAME: $DB_NAME"
+#echo "DB_PASS: $DB_PASSWORD"
+#echo "DB_HOST: $DB_HOST"
 
-DB_NAME=$(grep -E "^define\('DB_NAME'" "$WP_CONFIG" |  cut -d"'" -f 4)
-DB_USER=$(grep -E "^define\('DB_USER'" "$WP_CONFIG" |  cut -d"'" -f 4)
-DB_PASSWORD=$(grep -E "^define\('DB_PASSWORD'" "$WP_CONFIG" |  cut -d"'" -f 4)
-DB_HOST=$(grep -E "^define\('DB_HOST'" "$WP_CONFIG" | cut -d"'" -f 4)
+mysqldump  -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > /home/ubuntu/backup/mysqldump.sql
+zip -r  /home/ubuntu/backup/Wordpress.zip "$WP_FOLDER" /home/ubuntu/backup/mysqldump.sql
+aws s3  cp /home/ubuntu/backup/Wordpress.zip s3://store-zip-file
 
-echo "DB_USER: $DB_USER"
-echo "DB_NAME: $DB_NAME"
-echo "DB_PASS: $DB_PASSWORD"
-echo "DB_HOST: $DB_HOST"
-
-mysqldump  -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" > /home/scripts/mysqldump.sql
-zip  Wordpress.zip "$WP_FOLDER" /home/scripts/mysqldump.sql
 
 
 
